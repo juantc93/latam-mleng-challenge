20d7311c9a8cdd9becd149d4db166e6ba3d6321a
ext_jjotoroc <ext_jjotoroc@falabella.cl>
2023-04-27 14:08:02 -0500

    first commit

---
+++ app/main.py
@@ -0,0 +1,27 @@
+import joblib
+import numpy as np
+from src.models.predict import Model  as PredictRequest
+from fastapi import FastAPI
+from typing import List
+ 
+
+#import ml model
+model = joblib.load("models/model_v0.pkl")
+
+def predict(input_data):
+    # Do your prediction here using the loaded model and input data
+    prediction = model.predict(input_data)
+    return prediction
+
+
+
+app = FastAPI()
+
+
+
+@app.post("/predict")
+def make_prediction(request: PredictRequest):
+    input_data = np.array(list(request.dict().values())).reshape((1,-1))
+    prediction = predict(input_data) # Note that we pass a list of lists to the predict function
+    return {"prediction": int(prediction[0])}
+    #return {"input_data":str(list(input_data))}
